fdb1ca263f96b21e1960b28f07b2337744a4d664
Igor602437 <a505741747@gmail.com>
2025-08-14 18:16:56 +0300

    fix-err

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -7,12 +7,15 @@ axios.defaults.headers.common['Authorization'] = `Bearer ${API_KEY}`;
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 interface TmdbResponse {
-  // page: number;
+  page: number;
   results: Movie[];
   total_pages: number;
 }
 
-export const fetchMovies = async (query: string, page: number) => {
+export const fetchMovies = async (
+  query: string,
+  page: number
+): Promise<TmdbResponse> => {
   const axiosOptions = {
     params: {
       page,
